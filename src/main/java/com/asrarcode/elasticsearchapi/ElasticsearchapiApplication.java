9cee34e379eaeb48d20c6afc56583d175077e7d8
ASRAR_HP_VICTUS\asrar2602 <asrar60@gmail.com>
2024-06-28 01:26:03 +0530

    initial commit

---
+++ src/main/java/com/asrarcode/elasticsearchapi/ElasticsearchapiApplication.java
@@ -0,0 +1,13 @@
+package com.asrarcode.elasticsearchapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticsearchapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElasticsearchapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/asrarcode/elasticsearchapi/ElasticsearchapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asrarcode.elasticsearchapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElasticsearchapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
